b9de37c937480f1992f31a1bc7a9bc45e436f03d
sadevopseng <sravan.anm@gmail.com>
2020-09-25 07:19:01 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'tree' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+   content 'This server is the property of GOOGLE'
+   action :create
+   owner 'root'
+   group 'root'
+end
+
+
+
+
+
+  
